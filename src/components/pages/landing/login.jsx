94a979844207a3c975fc13963ac7696366a1a12c
Joe Wang <92607615+Pancakes12315@users.noreply.github.com>
2025-08-13 19:58:58 -0400

    Update login.jsx
    o Created the layout of the login popup
    o Incorporated the brand logo + Main/Sub greeting messages

--- src/components/pages/landing/login.jsx
+++ src/components/pages/landing/login.jsx
@@ -1,1 +1,34 @@
-ls jknajskdnajsdnasjkdnsajd
+import React from "react";
+import Logo from "./logo";
+import Greeting from "./greeting";
+
+const Login: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#050222]">
+      <div
+        className="
+          rounded-[25px] 
+          border-[3px] border-white/30 
+          bg-white/10 
+          backdrop-blur-[125px] 
+          p-10
+          w-[50%] 
+          max-w-full
+          h-[650px]
+          flex flex-col items-center
+        "
+      >
+        {/* Logo at the top */}
+        <Logo />
+
+        {/* Greeting text */}
+        <Greeting />
+        
+        {/* Login content goes here */}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
