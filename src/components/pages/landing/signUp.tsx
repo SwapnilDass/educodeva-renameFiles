7ec22d626340f3b280b0d7d6f96c3e2af06d8cad
Joe Wang <92607615+Pancakes12315@users.noreply.github.com>
2025-08-14 14:34:48 -0400

    Update signUp.tsx

--- src/components/pages/landing/signUp.tsx
+++ src/components/pages/landing/signUp.tsx
@@ -1,31 +1,12 @@
 import React from "react";
-import "../../../styles/orDivider.css"; // updated path for your folder structure
+import "../../../styles/loginButton.css"; // updated path for your folder structure
 
-const OrDivider: React.FC = () => {
+const LoginButton: React.FC = () => {
   return (
-    <div className="or-divider-container">
-      {/* Left line */}
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 934 5" className="or-line" fill="none">
-        <path
-          d="M2.08368 0.857423C1.25526 0.85651 0.582945 1.52734 0.582032 2.35577C0.58112 3.1842 1.25195 3.85651 2.08038 3.85742L2.08203 2.35742L2.08368 0.857423ZM2.08203 2.35742L2.08038 3.85742L933.793 4.88382L933.794 3.38382L933.796 1.88382L2.08368 0.857423L2.08203 2.35742Z"
-          stroke="rgba(255,255,255,0.31)"
-          strokeWidth="3"
-        />
-      </svg>
-
-      {/* Or Text */}
-      <span className="or-text">Or</span>
-
-      {/* Right line */}
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 934 5" className="or-line" fill="none">
-        <path
-          d="M2.08368 0.857423C1.25526 0.85651 0.582945 1.52734 0.582032 2.35577C0.58112 3.1842 1.25195 3.85651 2.08038 3.85742L2.08203 2.35742L2.08368 0.857423ZM2.08203 2.35742L2.08038 3.85742L933.793 4.88382L933.794 3.38382L933.796 1.88382L2.08368 0.857423L2.08203 2.35742Z"
-          stroke="rgba(255,255,255,0.31)"
-          strokeWidth="3"
-        />
-      </svg>
-    </div>
+    <button className="login-button">
+      Log In
+    </button>
   );
 };
 
-export default OrDivider;
+export default LoginButton;
