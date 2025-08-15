c9c152e151e5f2562a2269242bc9d3dfe5a22396
Joe Wang <92607615+Pancakes12315@users.noreply.github.com>
2025-08-15 12:56:48 -0400

    Update signUp.tsx

--- src/components/pages/landing/signUp.tsx
+++ src/components/pages/landing/signUp.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import "../../../styles/loginButton.css"; // updated path for your folder structure
+import "../../../styles/signUp.css"; // updated path for your folder structure
 
-const LoginButton: React.FC = () => {
+const Signup: React.FC = () => {
   return (
-    <button className="login-button">
-      Log In
-    </button>
+    <div className="signup-container">
+      {/* Left Text */}
+      <span className="signup-left-text">
+        Don't have an account?
+      </span>
+
+      {/* Right Text as Button */}
+      <button
+        className="signup-right-button"
+        onClick={() => {
+          console.log("Sign Up Now clicked!");
+          // Add your navigation or signup logic here
+        }}
+      >
+        Sign Up Now!
+      </button>
+    </div>
   );
 };
 
-export default LoginButton;
+export default Signup;
