b037913a872217d4529cbeb64757f7abac95dfc7
Joe Wang <92607615+Pancakes12315@users.noreply.github.com>
2025-08-15 13:59:19 -0400

    Update login.tsx

--- src/components/pages/landing/login.tsx
+++ src/components/pages/landing/login.tsx
@@ -6,12 +6,13 @@ import PassField from "./PassField";
 import LoginButton from "./loginButton";
 import SignUp from "./signUp";
 import OrDivider from "./OrDivider";
+import SocialButtons from "./SocialButtons";
 
 const Login: React.FC = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-[#050222]">
       {/*Gray Layout*/}
-      <div className="flex h-[650px] w-[50%] max-w-full flex-col items-center rounded-[25px] border-[3px] border-white/30 bg-white/10 p-10 backdrop-blur-[125px]">
+      <div className="flex h-[750px] w-[50%] max-w-full flex-col items-center rounded-[25px] border-[3px] border-white/30 bg-white/10 p-6 backdrop-blur-[125px]">
         {/* Logo at the top */}
         <Logo />
 
@@ -28,6 +29,9 @@ const Login: React.FC = () => {
 
         {/*Divider */}
         <OrDivider />
+
+        {/*Social Buttons */}
+        <SocialButtons />
       </div>
     </div>
   );
