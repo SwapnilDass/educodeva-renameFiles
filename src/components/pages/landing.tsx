13c635d275b7e2a6076cd11788858d41f88d9def
pouyan nazarzade <poyannazarzade@gmail.com>
2025-08-15 16:23:18 -0400

    added the cards for section 2

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -1,10 +1,12 @@
 import "../../styles/index.css";
 import Hero from "./landing/hero";
+import Events from "./landing/Events";
 
 export default function Landing() {
   return (
     <section id="landing" className="h-[100vh] w-[100vw] bg-black text-white">
       <Hero />
+      <Events />
     </section>
   );
 }
