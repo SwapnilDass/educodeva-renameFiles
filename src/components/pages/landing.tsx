483176c68f4b30614c2d0b98d9968bec76c46394
amchisa <chisa.alexander@gmail.com>
2025-08-15 13:29:18 -0400

    Revert: Restore Login component on landing page - Reverted the accidental commenting out of the <Login /> component.

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -5,7 +5,7 @@ import TestimonialSection from "./landing/section-3/TestimonialSection";
 export default function Landing() {
   return (
     <section id="landing" className="h-[100vh] w-[100vw] bg-black text-white">
-      {/* <Login /> */}
+      <Login />
 
       {/* Section 3 - Testimonials */}
       <TestimonialSection />
