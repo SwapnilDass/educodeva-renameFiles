9d437ae840e2895e4cdd21035c409ad7114afd7a
amchisa <chisa.alexander@gmail.com>
2025-08-15 13:23:22 -0400

    Refactoring and styling tweaks for testimonial section - Tweaked styling to more closely match figma design - Improved code comments - Refactored the plane icon into its own PlaneBadge component - Added standard HTML props to TestimonialSection for reusability

--- src/components/pages/landing/section-3/TestimonialCard.tsx
+++ src/components/pages/landing/section-3/TestimonialCard.tsx
@@ -1,34 +1,38 @@
 interface TestimonialCardProps {
-  name: string;
+  username: string;
   handle: string;
-  image?: string;
   body: string;
+  pfp?: string;
+  image?: string;
 }
 
 export default function TestimonialCard({
-  name,
+  username,
   handle,
-  image,
   body,
+  pfp,
+  image,
 }: TestimonialCardProps) {
   return (
     <div className="break-inside-avoid rounded-xl border border-white/15 bg-white/10 px-5 py-4">
       <div className="space-y-4">
+        {/* User information (pfp, username, handle) */}
         <div className="flex items-center gap-4">
           <div className="h-12 w-12 rounded-full bg-white">
-            <img className=""></img>
+            {pfp && <img src={pfp}></img>}
           </div>
           <div>
-            <p>{name}</p>
-            <p className="text-sm text-gray-300">{handle}</p>
+            <p className="text-sm">{username}</p>
+            <p className="text-xs text-gray-300">{handle}</p>
           </div>
         </div>
 
+        {/* Body text and (optional) image*/}
         <div className="space-y-4">
-          <p className="text-sm text-gray-300">{body}</p>
+          <p className="text-xs text-gray-300">{body}</p>
           {image !== undefined && (
-            <div className="h-60 w-full rounded-xl bg-white">
-              <img></img>
+            <div className="h-[13.75rem] w-full rounded-xl bg-white">
+              {image && <img src={image}></img>}
             </div>
           )}
         </div>
--- src/components/pages/landing/section-3/TestimonialSection.tsx
+++ src/components/pages/landing/section-3/TestimonialSection.tsx
@@ -1,32 +1,28 @@
+import { HTMLAttributes } from "react";
+import PlaneBadge from "./PlaneBadge";
 import TestimonialGrid from "./TestimonialGrid";
 
-export default function TestimonialSection() {
+// Accept typical HTML <section> attributes (like className) for reusability
+interface TestimonialSectionProps extends HTMLAttributes<HTMLElement> {}
+
+export default function TestimonialSection({
+  className,
+  ...props
+}: TestimonialSectionProps) {
   return (
-    <section className="flex flex-col items-center justify-center bg-[#050222]">
-      {/* Content container */}
+    <section
+      className={`flex flex-col items-center justify-center bg-[#050222] ${className}`}
+      {...props} // Other HTML <section> attributes
+    >
       <div className="h-[85%] w-[80%]">
         {/* Header */}
-        <div className="mb-16 flex h-fit gap-10">
-          {/* Icon */}
-          <div className="ml-2 h-16 w-16 flex-shrink-0 rounded-[10px] bg-[#ED066B] p-[18px]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="149"
-              height="149"
-              viewBox="0 0 149 149"
-              fill="none"
-              className="h-full w-full"
-            >
-              <path
-                d="M84.198 92.2564L59.7102 114.04L63.4688 131.289L45.7579 149L27.9118 121.087L0 103.241L17.7109 85.5292L35.8988 90.2276L56.7347 64.7923L12.8062 35.5057L32.9374 15.3668L87.864 33.6833L114.865 6.68191C123.2 -1.66127 136.113 -2.27348 143.695 5.30799C151.276 12.8895 150.657 25.7958 142.321 34.1389L115.32 61.1404L133.622 116.047L113.491 136.186L84.2051 92.2564H84.198Z"
-                fill="white"
-              />
-            </svg>
-          </div>
+        <div className="mb-16 flex h-fit items-center gap-12">
+          {/* Badge */}
+          <PlaneBadge />
 
           {/* Title and description */}
           <div className="flex h-full flex-col justify-between">
-            <h2 className="text-[1.625rem] font-medium">
+            <h2 className="text-[1.7rem] font-medium">
               Bringing students together,{" "}
               <span
                 style={{
@@ -41,14 +37,14 @@ export default function TestimonialSection() {
               </span>
             </h2>
 
-            <p className="text-[1.15rem] font-normal text-white/70">
+            <p className="text-[1.2rem] font-normal text-white/70">
               Building the future of learning by making it accessible to
               students worldwide.
             </p>
           </div>
         </div>
 
-        {/* User testimonials */}
+        {/* User reviews/testimonials */}
         <TestimonialGrid />
       </div>
     </section>
