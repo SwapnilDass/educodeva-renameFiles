4a79d70ec143097b4b813db5c5560010f4ad5d0f
Joe Wang <92607615+Pancakes12315@users.noreply.github.com>
2025-08-16 21:52:58 -0400

    Update landing.tsx

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -1,12 +1,10 @@
 import "../../styles/index.css";
 import Login from "./Login/login";
-import Signup from "./Signup/signup";
 
 export default function Landing() {
   return (
     <section id="landing" className="h-[100vh] w-[100vw] bg-black text-white">
       <Login />
-      <Signup />
     </section>
   );
 }
