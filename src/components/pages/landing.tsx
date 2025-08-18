aa8dd31108d482ac0cc602b6806862686a99f63c
Joe Wang <92607615+Pancakes12315@users.noreply.github.com>
2025-08-17 23:51:55 -0400

    Update landing.tsx

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -1,10 +1,11 @@
 import "../../styles/index.css";
 import Login from "./Login/login";
+import FooterPage from "./Footer/FooterPage";
 
 export default function Landing() {
   return (
     <section id="landing" className="h-[100vh] w-[100vw] bg-black text-white">
-      <Login />
+      <FooterPage />
     </section>
   );
 }
