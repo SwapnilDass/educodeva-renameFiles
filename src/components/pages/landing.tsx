f2f4a776d34ca987f5168f855d726d29d2c68586
Swapnil Das <swapnildas212@gmail.com>
2025-08-21 10:22:11 -0400

    Updated Footer and Pop Ups

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -1,10 +1,17 @@
 import "../../styles/index.css";
-import Login from "./landing/login";
+import Hero from "./landing/hero";
+import Events from "./landing/section-2/Events";
+import Testimonials from "./landing/section-3/Testimonials";
+//import Login from "./Login/login";
+import FooterPage from "./Footer/FooterPage";
 
 export default function Landing() {
   return (
     <section id="landing" className="h-[100vh] w-[100vw] bg-black text-white">
-      <Login />
+      <Hero />
+      <Events />
+      <Testimonials />
+      <FooterPage />
     </section>
   );
 }
