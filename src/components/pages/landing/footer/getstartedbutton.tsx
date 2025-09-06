97c97e28af79b44f504b517575bd8bbdd9ed041a
Joe Wang <92607615+Pancakes12315@users.noreply.github.com>
2025-09-06 17:12:23 -0400

    Create getstartedbutton.tsx

---
+++ src/components/pages/landing/footer/getstartedbutton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../../../../styles/Footer-styles/getStartedButton.css";
+
+interface GetStartedButtonProps {
+  label?: string;
+}
+
+const GetStartedButton: React.FC<GetStartedButtonProps> = ({
+  label = "Get Started",
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      className="get-started-button"
+      onClick={() => navigate("/login")} // redirect to login
+    >
+      {label}
+    </button>
+  );
+};
+
+export default GetStartedButton;
