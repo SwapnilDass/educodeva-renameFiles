4b5a9dc401755a24a36296d9c3f3a4fb7873d66b
Joe Wang <92607615+Pancakes12315@users.noreply.github.com>
2025-09-06 17:15:18 -0400

    Update landing.tsx

--- src/components/pages/landing/landing.tsx
+++ src/components/pages/landing/landing.tsx
@@ -1,24 +1,22 @@
 import "../../../styles/index.css";
 import Hero from "./hero/hero";
-import Learning from "./section-1/Learning";
+import Learning from "./section-1/Learning"
 import Events from "./section-2/Events";
 import Testimonials from "./section-3/Testimonials";
-import FooterPage from "../Footer/FooterPage";
+//import Login from "./Login/login";
+import FooterPage from "./footer/footerpage";
 import Partners from "./partners/Partners";
 
 export default function Landing() {
   return (
-    <section
-      id="landing"
-      className="h-[100vh] w-[100vw] bg-[#050222] text-white"
-    >
-      <div className="overflow-hidden bg-inherit px-4 pt-4">
+    <section id="landing" className="h-[100vh] w-[100vw] bg-[#050222] text-white">
+      <div className="px-4 pt-4 overflow-hidden h-full ">
         <Hero />
       </div>
       <Partners />
       <Learning />
-      <Events className="py-12" />
-      <Testimonials className="py-12" />
+      <Events />
+      <Testimonials />
       <FooterPage />
     </section>
   );
